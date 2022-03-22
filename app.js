0c069343cb4e902aba3eef9c6628820e3ed032dc
pengpeng <s123171293@gmail.com>
2022-03-22 11:40:43 +0800

    little change

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let name = "daniel";
 
-let gretting = require("./gretting");
-
+const gretting = require("./gretting");
+const path = require("path");
 let sayhi = () => {
     console.log("Hello "+ name);
 };
 
+console.log(path.join(__dirname,"try.js"))
 sayhi();
 gretting.morning(name);
 gretting.night(name);
